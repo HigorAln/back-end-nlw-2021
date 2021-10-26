fe941f52ba3ce83ae9693ea9149df553c39a0f1b
Higor <higor.allan21@gmail.com>
2021-10-26 10:50:46 -0300

    back-end nlw 2021

---
+++ readme.md
@@ -0,0 +1,1 @@
+### aplicacao back-end feita no nwl-2021,</br> desenvolvida em nodejs and express, pra autententicacao com o github
---
+++ src/services/GetLast3MessageService.ts
@@ -0,0 +1,23 @@
+import { prisma } from '.prisma/client';
+import prismaClient from '../prisma/index';
+
+
+class GetLast3MessageService {
+    async execute(){
+       const messages = await prismaClient.message.findMany({
+           take: 3,
+           orderBy: {
+               created_at: "desc"
+           },
+           include:{
+               user: true
+           }
+       })
+       
+       // SELECT * FROM MESSAGES LIMIT 3 ORDER BY CREATE_AT DESC
+
+       return messages
+    }
+}
+
+export { GetLast3MessageService }
